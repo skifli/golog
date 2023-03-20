4318f27dfef46ad7718163733d9042a56fc8c009
skifli <121291719+skifli@users.noreply.github.com>
2023-03-20 13:31:53 +0000

    feat: make API simpler

--- examples/simple_example.go
+++ examples/simple_example.go
@@ -21,7 +21,7 @@ func main() {
 	logFile, err := os.OpenFile("simple_example.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
 
 	if err != nil {
-		logger.Panic(err, nil)
+		logger.Panic(err)
 	}
 
 	// Add the file log to the logger.
@@ -35,18 +35,18 @@ func main() {
 	// Will be run after the 'logger.Panic' call.
 	defer func() {
 		if r := recover(); r != nil {
-			logger.Infof("recovered from the panic '%v'", nil, r)
-			logger.Fatal("and this is a fatal error", nil)
+			logger.Infof("recovered from the panic '%v'", r)
+			logger.Fatal("and this is a fatal error")
 		}
 	}()
 
-	logger.Debugf("this is a %s message", nil, "debug")
-	logger.Info("some useful text", nil)
-	logger.Warningf(
+	logger.Debugf("this is a %s message", "debug")
+	logger.Info("some useful text")
+	logger.WarningFieldsf(
 		"this is a %s message - you can even add fields",
 		golog.Fields{"service": "golog"},
 		"warning",
 	)
-	logger.Error("this is an error", nil)
-	logger.Panic(errors.New("this is a panic on an error"), nil)
+	logger.Error("this is an error")
+	logger.Panic(errors.New("this is a panic on an error"))
 }
--- log.go
+++ log.go
@@ -29,75 +29,143 @@ func (logger *Logger) log(levelIndex levelIndexes, fields Fields, msg string) {
 	}
 }
 
+// Logs a message, using FATAL as the log level.
+// Then calls 'os.Exit(1)'.
+func (logger *Logger) Fatal(msg string) {
+	logger.log(FATAL, nil, msg)
+	os.Exit(1)
+}
+
 // Logs a message with the specified fields, using FATAL as the log level.
 // Then calls 'os.Exit(1)'.
-func (logger *Logger) Fatal(msg string, fields Fields) {
+func (logger *Logger) FatalFields(msg string, fields Fields) {
 	logger.log(FATAL, fields, msg)
+	os.Exit(1)
+}
+
+// Formats the message, using FATAL as the log level. Then calls 'os.Exit(1)'.
+func (logger *Logger) Fatalf(msg string, format ...any) {
+	logger.log(FATAL, nil, fmt.Sprintf(msg, format...))
 	os.Exit(1)
 }
 
 // Formats the message, and then logs it with the specified fields, using FATAL
 // as the log level. Then calls 'os.Exit(1)'.
-func (logger *Logger) Fatalf(msg string, fields Fields, format ...any) {
+func (logger *Logger) FatalFieldsf(msg string, fields Fields, format ...any) {
 	logger.log(FATAL, fields, fmt.Sprintf(msg, format...))
 	os.Exit(1)
 }
 
 // Logs the error, using ERROR as the log level. Then calls 'panic(err)'.
-func (logger *Logger) Panic(err error, fields Fields) {
+func (logger *Logger) Panic(err error) {
+	logger.log(ERROR, nil, err.Error())
+	panic(err)
+}
+
+// Logs the error with the specified fields, using ERROR as the log level.
+// Then calls 'panic(err)'.
+func (logger *Logger) PanicFields(err error, fields Fields) {
 	logger.log(ERROR, fields, err.Error())
+	panic(err)
+}
+
+// Formats the message, using ERROR as the log level. Then calls 'panic(err)'.
+func (logger *Logger) Panicf(msg string, format ...any) {
+	err := fmt.Sprintf(msg, format...)
+
+	logger.log(ERROR, nil, err)
 	panic(err)
 }
 
 // Formats the message, and then logs it with the specified fields, using ERROR
 // as the log level. Then calls 'panic(err)'.
-func (logger *Logger) Panicf(msg string, fields Fields, format ...any) {
+func (logger *Logger) PanicFieldsf(msg string, fields Fields, format ...any) {
 	err := fmt.Sprintf(msg, format...)
 
 	logger.log(ERROR, fields, err)
 	panic(err)
 }
 
+// Logs a message, using ERROR as the log level.
+func (logger *Logger) Error(msg string) {
+	logger.log(ERROR, nil, msg)
+}
+
 // Logs a message with the specified fields, using ERROR as the log level.
-func (logger *Logger) Error(msg string, fields Fields) {
+func (logger *Logger) ErrorFields(msg string, fields Fields) {
 	logger.log(ERROR, fields, msg)
+}
+
+// Formats the message, using ERROR as the log level.
+func (logger *Logger) Errorf(msg string, format ...any) {
+	logger.log(ERROR, nil, fmt.Sprintf(msg, format...))
 }
 
 // Formats the message, and then logs it with the specified fields, using ERROR
 // as the log level.
-func (logger *Logger) Errorf(msg string, fields Fields, format ...any) {
+func (logger *Logger) ErrorFieldsf(msg string, fields Fields, format ...any) {
 	logger.log(ERROR, fields, fmt.Sprintf(msg, format...))
 }
 
+// Logs a message, using WARNING as the log level.
+func (logger *Logger) Warning(msg string) {
+	logger.log(WARNING, nil, msg)
+}
+
 // Logs a message with the specified fields, using WARNING as the log level.
-func (logger *Logger) Warning(msg string, fields Fields) {
+func (logger *Logger) WarningFields(msg string, fields Fields) {
 	logger.log(WARNING, fields, msg)
+}
+
+// Formats the message, using WARNING as the log level.
+func (logger *Logger) Warningf(msg string, format ...any) {
+	logger.log(WARNING, nil, fmt.Sprintf(msg, format...))
 }
 
 // Formats the message, and then logs it with the specified fields, using WARNING
 // as the log level.
-func (logger *Logger) Warningf(msg string, fields Fields, format ...any) {
+func (logger *Logger) WarningFieldsf(msg string, fields Fields, format ...any) {
 	logger.log(WARNING, fields, fmt.Sprintf(msg, format...))
 }
 
+// Logs a message, using INFO as the log level.
+func (logger *Logger) Info(msg string) {
+	logger.log(INFO, nil, msg)
+}
+
 // Logs a message with the specified fields, using INFO as the log level.
-func (logger *Logger) Info(msg string, fields Fields) {
+func (logger *Logger) InfoFields(msg string, fields Fields) {
 	logger.log(INFO, fields, msg)
+}
+
+// Formats the message, using INFO as the log level.
+func (logger *Logger) Infof(msg string, format ...any) {
+	logger.log(INFO, nil, fmt.Sprintf(msg, format...))
 }
 
 // Formats the message, and then logs it with the specified fields, using INFO
 // as the log level.
-func (logger *Logger) Infof(msg string, fields Fields, format ...any) {
+func (logger *Logger) InfoFieldsf(msg string, fields Fields, format ...any) {
 	logger.log(INFO, fields, fmt.Sprintf(msg, format...))
 }
 
+// Logs a message, using DEBUG as the log level.
+func (logger *Logger) Debug(msg string) {
+	logger.log(DEBUG, nil, msg)
+}
+
 // Logs a message with the specified fields, using DEBUG as the log level.
-func (logger *Logger) Debug(msg string, fields Fields) {
+func (logger *Logger) DebugFields(msg string, fields Fields) {
 	logger.log(DEBUG, fields, msg)
+}
+
+// Formats the message, using DEBUG as the log level.
+func (logger *Logger) Debugf(msg string, format ...any) {
+	logger.log(DEBUG, nil, fmt.Sprintf(msg, format...))
 }
 
 // Formats the message, and then logs it with the specified fields, using DEBUG
 // as the log level.
-func (logger *Logger) Debugf(msg string, fields Fields, format ...any) {
+func (logger *Logger) DebugFieldsf(msg string, fields Fields, format ...any) {
 	logger.log(DEBUG, fields, fmt.Sprintf(msg, format...))
 }
